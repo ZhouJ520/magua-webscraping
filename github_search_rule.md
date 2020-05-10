32bc6fca160d8e9815b976b1e1cf2f7367472fad
ZhouJ520 <zhoujian_1986@126.com>
2020-05-10 21:27:26 +0800

    第二课笔记及代码

--- github_search_rule.md
+++ github_search_rule.md
@@ -1,4 +1,7 @@
 Search repositories(https://developer.github.com/v3/search/):
 
-搜索api：https://api.github.com/search/repositories?q=python&
+搜索api：https://api.github.com/search/repositories?q=topic：python&sort=stars&order=desc
 
+![](https://i.loli.net/2020/05/10/fFGIipnrQbXhMjg.png)
+
+可用 postman 直接搜索
--- lesson2_01.py
+++ lesson2_01.py
@@ -7,5 +7,33 @@
 # @Function:
 # @Software: PyCharm
 
+# https://api.github.com/search/repositories?q=django
+# https://api.github.com/search/repositories?q=topic:django
+
+
 import requests
 import webbrowser
+
+
+def get_names():
+    names = input("Separate each name with Space:")
+    return names.split()
+
+
+def check_repos(names):
+    repo_api = 'https://api.github.com/search/repositories?q='
+    ecosys_api = 'https://api.github.com/search/repositories?q=topic:'
+    for name in names:
+        repo_info = requests.get(repo_api + name).json()['items'][0]
+        stars = repo_info['stargazers_count']
+        forks = repo_info['forks_count']
+        ecosys_info = requests.get(ecosys_api + name).json()['total_count']
+        print(name)
+        print('Stars:', stars)
+        print('Forks:', forks)
+        print('Ecosys:', ecosys_info)
+        print("---------------")
+
+
+names = get_names()
+check_repos(names)
